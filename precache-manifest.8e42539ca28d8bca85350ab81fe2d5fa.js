862070e3a0404f00967039a8fcd5d83fca1241f0
muthimenijyoshna7095 <muthimenijyoshna7095@gmail.com>
2025-03-20 22:18:55 +0530

    Updates

---
+++ precache-manifest.8e42539ca28d8bca85350ab81fe2d5fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a3e5f0ec01f923edf178054a9d35d13",
+    "url": "/myaiapp-frontend/index.html"
+  },
+  {
+    "revision": "fbfe0a7b1f3662ff4719",
+    "url": "/myaiapp-frontend/static/css/main.a261e941.chunk.css"
+  },
+  {
+    "revision": "d0229e7b58a604e1c2b0",
+    "url": "/myaiapp-frontend/static/js/2.19a0bb35.chunk.js"
+  },
+  {
+    "revision": "73e88762c69a3c614405",
+    "url": "/myaiapp-frontend/static/js/3.c02789ba.chunk.js"
+  },
+  {
+    "revision": "fbfe0a7b1f3662ff4719",
+    "url": "/myaiapp-frontend/static/js/main.343eb75e.chunk.js"
+  },
+  {
+    "revision": "c0af002b95e873e4556e",
+    "url": "/myaiapp-frontend/static/js/runtime~main.69a5db70.js"
+  }
+]);
